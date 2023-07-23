53889b13e74757d8788727809aa67353dc640a7b
Rahul Ghangas <rahulghangas0@gmail.com>
2023-07-23 12:49:15 +0530

    chore: update exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { UserProof } from './circuits/user/UserProof.js';
+import { AggregatorProof } from './circuits/aggregator/AggregatorProof.js';
 
+export { AggregatorProof };
 export { UserProof };
